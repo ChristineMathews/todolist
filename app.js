7127b98ce481e820fa2c156fe88f7280e9713c83
Christine Mathews <christinemathews@Christines-MacBook-Air.local>
2020-05-10 00:10:44 +0530

    Added MongoDB

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express=require("express");
 const bodyParser=require("body-parser");
+const mongoose=require("mongoose");
 
 const app = express();
 
@@ -7,17 +8,37 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
+mongoose.connect("mongodb//localhost:27017/todolistdb",{useNewUrlParser:true});
+const itemSchema={
+    name:String
+};
+const Item=mongoose.model("Item",itemSchema);
+const Item1=new Item({
+    name:"Welcome to do list!"
+});
+const Item2=new Item({
+    name:"Click + to add to the list"
+});
+const defaultItems= [Item1,Item2];
+Item.insertMany(defaultItems,function(err){
+    if(err){console.log(err);}
+    else{console.log("succes");}
+});
+
+
 let items=[];
-
-
 app.get("/",function(req,res){
-    var today=new Date();
-    var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-    var day  = today.toLocaleDateString("en-US",options);
-    res.render("list",{
-        kindOfDay:day,
-        newListItems:items
+    // var today=new Date();
+    // var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    // var day  = today.toLocaleDateString("en-US",options);
+    Item.find({},function(err,foundItems){
+         console.log(foundItems); 
+         res.render("list",{
+            listTitle:"today",
+            newListItems:foundItems
+        });  
     });
+   
 
 
 
